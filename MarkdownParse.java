f561f556f69d839ca95943f9adf69fed75c4f603
Monip1 <elena@ratmotor.com>
2022-01-19 15:48:48 -0800

    displays image link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,19 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if(nextOpenBracket == 0){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+                // System.out.println(currentIndex);
+            }
+            else{
+                
+            }
         }
         return toReturn;
     }
