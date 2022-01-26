c40ee6b6b908c4f01aa86762d96ace4e7aaf2713
Monip1 <elena@ratmotor.com>
2022-01-26 15:41:44 -0800

    got junit tests working

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+//javac -cp "lib/\*" MarkdownParseTest.javajava -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+//javac -cp "lib/\*" MarkdownParseTest.javajava -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
     @Test
     public void addition() {
