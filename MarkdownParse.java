5e0078d7fccb75ccd18e2d520722f2c3d355cd1d
Monip1 <elena@ratmotor.com>
2022-03-09 15:25:04 -0800

    lab week10

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,93 +1,54 @@
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
+    public static ArrayList<String> getLinks(String arg) throws IOException {
+        Path fileName = Path.of(arg);
+	    String markdown = Files.readString(fileName);
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            // System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
+            
+            //if one element is not found in the rest of the file, break
+            if(nextOpenBracket < 0 || nextCloseBracket < 0 || openParen < 0 || closeParen < 0){
+                break;
             }
-            String potentialLink = markdown.substring(openParen + 1, closeParen);
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
+            else if(openParen - 1 != nextCloseBracket){
+                nextOpenBracket = markdown.indexOf("[", nextOpenBracket);
+                nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            }
+            else if(nextOpenBracket == 0){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
+                // System.out.println(currentIndex);
             }
-            else {
-                currentIndex = currentIndex + 1;
+            else if(!markdown.substring(nextOpenBracket - 1, nextOpenBracket).equals("!")){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+                // System.out.println(currentIndex);
             }
+            currentIndex = closeParen + 1;
+            // System.out.println(currentIndex);
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
+		
+        ArrayList<String> links = getLinks(args[0]);
         System.out.println(links);
     }
 }
-// // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-// import java.io.IOException;
-// import java.nio.file.Files;
-// import java.nio.file.Path;
-// import java.util.ArrayList;
-
-// public class MarkdownParse {
-//     public static ArrayList<String> getLinks(String arg) throws IOException {
-//         Path fileName = Path.of(arg);
-// 	    String markdown = Files.readString(fileName);
-//         ArrayList<String> toReturn = new ArrayList<>();
-//         // find the next [, then find the ], then find the (, then take up to
-//         // the next )
-//         int currentIndex = 0;
-//         while(currentIndex < markdown.length()) {
-//             // System.out.println(currentIndex);
-//             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-//             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-//             int openParen = markdown.indexOf("(", nextCloseBracket);
-//             int closeParen = markdown.indexOf(")", openParen);
-            
-//             //if one element is not found in the rest of the file, break
-//             if(nextOpenBracket < 0 || nextCloseBracket < 0 || openParen < 0 || closeParen < 0){
-//                 break;
-//             }
-//             else if(openParen - 1 != nextCloseBracket){
-//                 nextOpenBracket = markdown.indexOf("[", nextOpenBracket);
-//                 nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-//             }
-//             else if(nextOpenBracket == 0){
-//                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-//                 currentIndex = closeParen + 1;
-//                 // System.out.println(currentIndex);
-//             }
-//             else if(!markdown.substring(nextOpenBracket - 1, nextOpenBracket).equals("!")){
-//                 toReturn.add(markdown.substring(openParen + 1, closeParen));
-//                 currentIndex = closeParen + 1;
-//                 // System.out.println(currentIndex);
-//             }
-//             currentIndex = closeParen + 1;
-//             // System.out.println(currentIndex);
-//         }
-//         return toReturn;
-//     }
-//     public static void main(String[] args) throws IOException {
-		
-//         ArrayList<String> links = getLinks(args[0]);
-//         System.out.println(links);
-//     }
-// }
 
 
 // /**
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -2,26 +2,43 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
+// import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.List;
 
-//javac -cp "lib/\*" MarkdownParseTest.javajava -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+//javac -cp "lib//*" MarkdownParseTest.javajava -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
 public class MarkdownParseTest {
+    // @Test
+    // public void addition() {
+    //     assertEquals(2, 1 + 1);
+    // }
+
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void testSnip1() throws IOException{
+        List<String> expect = List.of("`google.com","google.com","ucsd.edu");
+        assertEquals(expect, MarkdownParse.getLinks("C:/Users/ElenaTomson/OneDrive - Tomson Family/Documents/GitHub/markdown-parse/snip1.md"));
+    }
+
+    @Test
+    public void testSnip2() throws IOException{
+        List<String> expect = List.of("a.com","a.com(())","example.com");
+        assertEquals(expect, MarkdownParse.getLinks("C:/Users/ElenaTomson/OneDrive - Tomson Family/Documents/GitHub/markdown-parse/snip2.md"));
     }
     @Test
-    public void testSpaceAfterParen() {
-        String contents = "[title]( space-in-url.com)";
-        List<String> expect = List.of("space-in-url.com");
-        assertEquals(expect, MarkdownParse.getLinks(contents));
+    public void testSnip3() throws IOException{
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expect, MarkdownParse.getLinks("C:/Users/ElenaTomson/OneDrive - Tomson Family/Documents/GitHub/markdown-parse/snip3.md"));
     }
     // @Test
+    // public void testSpaceAfterParen() {
+    //     String contents = "[title]( space-in-url.com)";
+    //     List<String> expect = List.of("space-in-url.com");
+    //     assertEquals(expect, MarkdownParse.getLinks(contents));
+    // }
+    // @Test
     // public void links()  throws IOException{
-    //     assertEquals(MarkdownParse.getLinks("test-file.md"), List.of("https://something.com", "some-page.html"));
+    //     assertEquals(MarkdownParse.getLinks(".\test-file.md"), List.of("https://something.com", "some-page.html"));
     //     assertEquals(4, 5);
     // }
     // @Test
